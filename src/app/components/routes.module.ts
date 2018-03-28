c59850d7e30d2060fff8c804dd0078c46ab67931
JJ <jared@langleyfoxall.co.uk>
2018-03-28 22:30:47 +0100

    Add font awesome, start of nav bar

--- src/app/components/routes.module.ts
+++ src/app/components/routes.module.ts
@@ -1,6 +1,4 @@
 import { RouterModule }           from '@angular/router';
-
-import { HomeComponent }          from './views/home.component';
 import { PreRegisterComponent  } from './views/pre-register.component';
 import { LoginComponent } from './views/login.component';
 import { AuthenticatedGuard } from '../guards/authenticated.guard';
@@ -9,6 +7,6 @@ import {FeedComponent} from './views/feed/feed.component';
 export const RoutesModule = RouterModule.forRoot([
   { path: '', component: PreRegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'feed', component: FeedComponent}
+  { path: 'feed', component: FeedComponent, canActivate: [ AuthenticatedGuard ]}
   ]
 );
